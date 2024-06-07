a5f6931f2dfec1f94e3cd19f1feda1fe71af4fcb
Yhawbee1 <antwibrian82@gmail.com>
2024-06-07 17:25:13 +0000

    Attempt to create authentication

---
+++ convex/auth.config.ts
@@ -0,0 +1,12 @@
+//https://willing-cicada-7.clerk.accounts.dev
+
+const authConfig = {
+    providers: [
+        {
+            domain: "https://willing-cicada-7.clerk.accounts.dev",
+            applicationID: "convex",
+        },
+    ]
+};
+
+export default authConfig;
