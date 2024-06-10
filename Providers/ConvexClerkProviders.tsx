a8d8baa3560fafc40aea262f92f9ffc8b0634fb6
Yhawbee1 <antwibrian82@gmail.com>
2024-06-10 11:30:50 +0000

    Create authentication

--- Providers/ConvexClerkProviders.tsx
+++ Providers/ConvexClerkProviders.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { ReactNode } from "react";
-import { ClerkProvider, useAuth } from "@clerk/clerk-react";
+import { ClerkProvider, useAuth } from "@clerk/nextjs";
 import { ConvexProviderWithClerk } from "convex/react-clerk";
 import { ConvexReactClient } from "convex/react";
 
@@ -14,6 +14,19 @@ export default function ConvexClientProvider({
   return (
     <ClerkProvider
       publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY as string}
+      appearance={{
+        layout: {
+          socialButtonsVariant: 'iconButton',
+          logoImageUrl: '/icons/auth-logo.svg'
+        },
+        variables: {
+          colorBackground: '#15171c',
+          colorPrimary: '',
+          colorText: 'white',
+          colorInputBackground: '#1b1f29',
+          colorInputText: 'white',
+        }
+      }}
     >
       <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
         {children}
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -10,9 +10,9 @@ const Home = () => {
     <div className="mb-2 flex flex-col gap-9">
       <section className="flex flex-col gap-5">
         <h1 className="text-20 font-bold text-white-1">Trending Podcast</h1>
-        <div className="flex flex-col items-center justify-between p-24">
+        {/* <div className="flex flex-col items-center justify-between p-24">
           {tasks?.map(({ _id, text }) => <div className="text-white-1" key={_id}>{text}</div>)}
-        </div>
+        </div> */}
         <div className="podcast_grid">
           {podcastData.map(({ id, title, description, imgURL }) => (
             <PodcastCard
